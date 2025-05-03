32c553437da6f79c12bf5a0480b361bf2f85f448
jfont01 <julian.font@mi.unc.edu.ar>
2025-05-03 20:04:02 -0300

    Se eliminan del repo los archivos ya versionados que ahora se van a ignorar

--- Debug/Core/Src/filters.d
+++
@@ -1,8 +0,0 @@
-Core/Src/filters.o: ../Core/Src/filters.c \
- ../Drivers/CMSIS/DSP/Include/arm_math.h \
- ../Drivers/CMSIS/Include/cmsis_compiler.h \
- ../Drivers/CMSIS/Include/cmsis_gcc.h ../Core/Inc/filters.h
-../Drivers/CMSIS/DSP/Include/arm_math.h:
-../Drivers/CMSIS/Include/cmsis_compiler.h:
-../Drivers/CMSIS/Include/cmsis_gcc.h:
-../Core/Inc/filters.h:
